689081b35d366acd1265883a50fce5341d97beed
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-10-24 21:15:15 +0530

    payment gateway completed

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.register, name="register"),    
+    path('', views.register, name="register"),
+    path('checkout/<str:tot>',views.payment,name="payment-gateway")  ,
+    path('creditcard/',views.credit_card,name="credit-card") ,
+    path('debitcard/',views.debit_card,name="debit-card"),
+    path('cod/',views.cod,name="cod")
+
 ]
